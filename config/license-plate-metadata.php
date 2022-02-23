d288222cc9ca113d30976a145c90e483a81e5b60
Simon <sdiaz.sz@gmail.com>
2022-02-22 21:34:45 -0300

    remove old config

--- config/license-plate-metadata.php
+++ config/license-plate-metadata.php
@@ -4,6 +4,5 @@ return [
     'route_prefix' => 'api/license-plate-metadata',
     'on_value_fail' => [
         'email_to_notify' => env('DEVELOPER_EMAIL', '')
-    ],
-    'migration_seed_from' => '2019-01-01'
+    ]
 ];
